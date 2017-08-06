fafd33ea32f84f8852277e2928faf4314bd90f02
Alvin Kong <alvink92@gmail.com>
2017-08-06 11:50:53 -0700

    Finished simon game

--- w2d1/simon/lib/simon.rb
+++ w2d1/simon/lib/simon.rb
@@ -4,38 +4,60 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    round_success_message
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep(1)
+      system("clear")
+    end
   end
 
   def require_sequence
-
+    @seq.each do |color|
+      guess = gets.chomp
+      if guess != color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.shuffle[0]
   end
 
   def round_success_message
-
+    puts "You have made it to the next round!"
   end
 
   def game_over_message
-
+    puts "Game over! You finished with a score of #{@sequence_length}"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
